0ec9f5b073a90c345f7626bf539ae4f7e83d645e
Ken H. Andersen <kha@aqua.dtu.dk>
2020-10-20 20:29:55 +0200

    Updated matlab chemostat

--- matlab/derivChemostat.m
+++ matlab/derivChemostat.m
@@ -1,8 +1,7 @@
 function dudt = derivChemostat(t, y,p)
-%y = [p.N0, p.DOC0, p.B0];
 dudt = 0*y;
 
 [tmp, tmp, tmp, tmp, tmp, dudt] = calllib('model','derivativeChemostat', ...
-    p.L, p.T, p.d, p.N0, y, dudt);
+    double(p.L), double(p.T), double(p.d), double(p.N0), y, dudt);
 
 end
