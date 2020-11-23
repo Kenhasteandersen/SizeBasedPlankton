755ea2a41744205a158a32cfee64836e2ca72beb
Ken H. Andersen <kha@aqua.dtu.dk>
2020-11-23 17:48:58 +0100

    pico-nano-micro in watercolumn

--- matlab/plotGlobalWatercolumn.m
+++ matlab/plotGlobalWatercolumn.m
@@ -11,11 +11,17 @@ subplot(1,2,1)
 loglog(sim.N(idx,iTime)/10,-z, 'b-','linewidth',2)
 hold on
 loglog(sim.DOC(idx,iTime),-z, 'm-','linewidth',2)
-loglog(sum(sim.B(idx,:,iTime),2),-z,'r-','linewidth',2)
+
+for i=1:length(idx)
+    Bpnm(i,:) = calcPicoNanoMicro(sim.B(idx(i),:,iTime), sim.p);
+end
+for i = 1:3
+    loglog(Bpnm(:,i),-z,'r-','linewidth',i)
+end
 loglog(sim.L(idx,iTime)+eps,-z,'y-','linewidth',2)
 ylim(-[max(z) min(z)])
 xlim([0.01,2000])
-legend({'N/10','DOC','B','Light'},'location','northoutside')
+legend({'N/10','DOC','Bpico','Bnano','Bmicro','Light'},'location','northoutside')
 ylabel('Depth (m)')
 xlabel('Concentration (\mu g)')
 
