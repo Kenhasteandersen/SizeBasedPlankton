5dc06380722ebce3b1559c91944b397ad4fc65d3
Ken H. Andersen <kha@aqua.dtu.dk>
2020-11-25 15:26:41 +0100

    dropped log axes

--- matlab/plotGlobalWatercolumn.m
+++ matlab/plotGlobalWatercolumn.m
@@ -8,9 +8,9 @@ end
 
 clf
 subplot(1,2,1)
-loglog(sim.N(idx,iTime)/10,-z, 'b-','linewidth',2)
+plot(sim.N(idx,iTime)/10,-z, 'b-','linewidth',2)
 hold on
-loglog(sim.DOC(idx,iTime),-z, 'm-','linewidth',2)
+plot(sim.DOC(idx,iTime),-z, 'm-','linewidth',2)
 
 for i=1:length(idx)
     Bpnm(i,:) = calcPicoNanoMicro(sim.B(idx(i),:,iTime), sim.p);
@@ -18,15 +18,17 @@ end
 for i = 1:3
     loglog(Bpnm(:,i),-z,'r-','linewidth',i)
 end
-loglog(sim.L(idx,iTime)+eps,-z,'y-','linewidth',2)
-ylim(-[max(z) min(z)])
+plot(sim.L(idx,iTime)+eps,-z,'y-','linewidth',2)
+ylim(-[max(z) 0])
 xlim([0.01,2000])
 legend({'N/10','DOC','Bpico','Bnano','Bmicro','Light'},'location','northoutside')
 ylabel('Depth (m)')
-xlabel('Concentration (\mu g)')
+xlabel('Concentration ({\mu}g/L)')
+set(gca,'xscale','log')
+
 
 subplot(1,2,2)
-surface(sim.p.m, -log10(z), squeeze(sim.B(idx,:,iTime)));
+surface(sim.p.m, -z, squeeze(sim.B(idx,:,iTime)));
 set(gca,'xscale','log')
 axis tight
 shading interp
--- matlab/plotGlobalWatercolumnTime.m
+++ matlab/plotGlobalWatercolumnTime.m
@@ -6,30 +6,30 @@ t = sim.t;
 
 clf
 subplot(3,1,1)
-surface(t,-log10(z), log10(double(sim.N(idx,:))))
-title(['Nitrogen, lat ', num2str(lat),', lon ', num2str(lon)])
-ylabel('log10 Depth (m)')
-set(gca,'yscale','log')
+surface(t,-z, log10(double(sim.N(idx,:))))
+title(['log10 Nitrogen, lat ', num2str(lat),', lon ', num2str(lon)])
+ylabel('Depth (m)')
+%set(gca,'yscale','log')
 shading interp
 axis tight
 colorbar
 
 subplot(3,1,2)
-surface(t,-log10(z), log10(double(sim.DOC(idx,:))))
-title(['DOC, lat ', num2str(lat),', lon ', num2str(lon)])
-ylabel('log10 Depth (m)')
+surface(t,-z, log10(double(sim.DOC(idx,:))))
+title(['log10 DOC, lat ', num2str(lat),', lon ', num2str(lon)])
+ylabel('Depth (m)')
 %xlabel('Concentration (\mugC l^{-1})')
-set(gca,'yscale','log')
+%set(gca,'yscale','log')
 shading interp
 axis tight
 colorbar
 
 subplot(3,1,3) 
-surface(t,-log10(z), squeeze(log10(sum(double(sim.B(idx,:,:)),2))))
-title(['Plankton, lat ', num2str(lat),', lon ', num2str(lon)])
-ylabel('log10 Depth (m)')
+surface(t,-z, squeeze(log10(sum(double(sim.B(idx,:,:)),2))))
+title(['log10 Plankton, lat ', num2str(lat),', lon ', num2str(lon)])
+ylabel('Depth (m)')
 xlabel('Time (days)')
-set(gca,'yscale','log')
+%set(gca,'yscale','log')
 shading interp
 axis tight
 colorbar
